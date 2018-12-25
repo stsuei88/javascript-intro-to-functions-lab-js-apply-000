bb007ef68f5a759f1123d93b5106c53dc705a0a4
Stephanie Tsuei <stsuei@buffalo.edu>
2018-12-25 13:17:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ function whisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string.toLowerCase()) === string){
+  if(string.toLowerCase() === string){
     return "I can't hear you!"
+  }
+  if(string.toUpperCase() === string){
+    return "YES INDEED!"
+  }
+  else{
+    return "I love you, too"
   }
 }
